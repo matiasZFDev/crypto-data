89756f4a652097c0d24a3637b520637f8e3f6e4a
matiaszfdev <cbudd333@gmail.com>
2022-04-02 14:20:22 -0300

    add: currencies and selected currency state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,59 @@
 import './index.css';
-// import LinkedList from '../../helpers/linked-list';
-import { useState, useEffect } from 'react';
+import Header from '../Header';
+import CryptoSelect from '../CryptoSelect';
+import CryptoCard from '../CryptoCard';
+import { useState, useEffect, } from 'react';
 
 const App = () => {
-	const [currencies, setCurrencies] = useState({});
+	const [currencies, setCurrencies] = useState(null);
+	const [currency, setCurrency] = useState(null);
 
 	useEffect(() => {
+		// fetch
+		const data = [
+			{
+				name: 'Bitcoin',
+				symbol: 'BTC',
+				id: 1,
+				total_market_cap: '1000',
+				volume: '1000',
+				total_supply: '19000000',
+				max_supply: '21000000',
+				changes: {
+					price: {
+						'24h': '-0.36',
+					},
+				},
+			},
+			{
+				name: 'Bitcoing',
+				symbol: 'BTC',
+				id: 2139,
+			},
+			{
+				name: 'Ethereum',
+				symbol: 'ETH',
+				id: 371,
+			},
+		];
+
+		const dataMap = new Map();
+		data.forEach((currency) => {
+			dataMap.set(currency.id, currency);
+		});
+		setCurrencies(dataMap);
+	}, []);
+
+	useEffect(() => {
+		if (currencies === null) return;
+
+		const random = Math.floor(Math.random() * currencies.size);
+		const keyArr = Array.from(currencies.keys());
+		const randomKey = keyArr[random];
+		setCurrency(currencies.get(randomKey));
+	}, [currencies]);
+
+	/*useEffect(() => {
 		const fetchCryptoList = async () => {
 			try {
 				const res = await fetch('http://localhost:8080');
@@ -22,14 +70,13 @@ const App = () => {
 		};
 
 		fetchCryptoList();
-	}, []);
-
-	useEffect(() => {
-		console.log(currencies);
-	}, [currencies]);
+	}, []);*/
 
 	return (
-    	<div className="App" id="app">
+    	<div id="app">
+			<Header />
+			<CryptoSelect currencies={currencies} currency={currency} setCurrency={setCurrency} />
+			<CryptoCard currency={currency} />
     	</div>
 	);
 }
