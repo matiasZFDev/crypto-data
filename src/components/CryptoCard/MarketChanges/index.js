e726b86a54f39f9f3d3f7d839aa18e15b672e35f
matiaszfdev <cbudd333@gmail.com>
2022-04-03 21:00:25 -0400

    remove: market cap change time periods

--- src/components/CryptoCard/MarketChanges/index.js
+++ src/components/CryptoCard/MarketChanges/index.js
@@ -7,7 +7,6 @@ const MarketChanges = ({ changes, }) => {
 			<h3>Changes</h3>
 			<div className="changes">
 				<CurrencyChange title="Price" values={changes.price} />
-				<CurrencyChange title="Market cap" values={changes.marketCap} />
 				<CurrencyChange title="Volume" values={changes.volume} />
 			</div>
 		</section>
