685eee3588549d56e3497e5435482a43081c60d6
matiaszfdev <cbudd333@gmail.com>
2022-04-07 18:55:20 -0400

    modify: dummy data to api data fetch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,96 +9,26 @@ const App = () => {
 	const [currency, setCurrency] = useState(null);
 
 	useEffect(() => {
-		// fetch
-		const data = [
-			{
-				name: 'Bitcoin',
-				symbol: 'BTC',
-				id: 1,
-				price: 49000,
-				marketCap: 823588,
-				volume: 56848646,
-				totalSupply: 19000000,
-				maxSupply: 21000000,
-				changes: {
-					price: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					marketCap: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					volume: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-				},
-			},
-			{
-				name: 'Bitcoing',
-				symbol: 'BTC',
-				id: 2139,
-				price: 49000,
-				marketCap: 823588,
-				volume: 56848646,
-				totalSupply: 19000000,
-				maxSupply: 21000000,
-				changes: {
-					price: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					marketCap: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					volume: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-				},
-			},
-			{
-				name: 'Ethereum',
-				symbol: 'ETH',
-				id: 371,
-				price: 49000,
-				marketCap: 823588,
-				volume: 56848646,
-				totalSupply: 19000000,
-				maxSupply: 21000000,
-				changes: {
-					price: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					marketCap: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-					volume: {
-						'24h': -0.36,
-						'7d': 9.34,
-						'30d': 54.16,
-					},
-				},
-			},
-		];
+		const fetchCryptoList = async () => {
+			try {
+				const res = await fetch('http://localhost:8080');
+				const json = await res.json();
 
-		const dataMap = new Map();
-		data.forEach((currency) => {
-			dataMap.set(currency.id, currency);
-		});
-		setCurrencies(dataMap);
+				if (!res.ok) {
+					throw new Error();
+				}
+
+				const dataMap = new Map();
+				json.forEach((currency) => {
+					dataMap.set(currency.id, currency);
+				});
+				setCurrencies(dataMap);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		fetchCryptoList();
 	}, []);
 
 	useEffect(() => {
@@ -109,25 +39,6 @@ const App = () => {
 		const randomKey = keyArr[random];
 		setCurrency(currencies.get(randomKey));
 	}, [currencies]);
-
-	/*useEffect(() => {
-		const fetchCryptoList = async () => {
-			try {
-				const res = await fetch('http://localhost:8080');
-				const json = await res.json();
-
-				if (!res.ok) {
-					throw new Error();
-				}
-
-				setCurrencies(json);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-
-		fetchCryptoList();
-	}, []);*/
 
 	return (
     	<div id="app">
