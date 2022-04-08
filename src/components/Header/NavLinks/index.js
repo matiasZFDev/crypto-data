75f4a6489c6ec8297ad53aa9436911262c8ead36
matiaszfdev <cbudd333@gmail.com>
2022-04-07 22:08:56 -0400

    add: responsive design

--- src/components/Header/NavLinks/index.js
+++ src/components/Header/NavLinks/index.js
@@ -5,8 +5,12 @@ const NavLinks = () => {
 		<nav id="links">
 			<ul>
 				<li>
-					<a href="https://github.com/matiasZFDev/crypto-data" target="_blank" rel="noreferrer">
-						<GithubLogo className="github-repository" />
+					<a 
+						href="https://github.com/matiasZFDev/crypto-data" 
+						target="_blank" 
+						rel="noreferrer"
+						title="Repo">
+							<GithubLogo className="github-repository" />
 					</a>
 				</li>
 			</ul>
